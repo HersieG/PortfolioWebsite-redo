1ed682a2b7757e356db5c3fa7fb04bc7b8a6acfe
HersieG <hersiedgonzalez@gmail.com>
2024-10-18 20:33:30 -0400

    testing 123

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const HeroSection = () => {
-  return <div>HeroSection</div>;
+  return <div className="h-[80vh] ">HeroSection</div>;
 };
 
 export default HeroSection;
